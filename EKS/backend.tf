4ca5358a10267df9d5c3f4985e7e9dc5f84e7d3e
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-07-04 09:36:30 +0700

    update backend s3

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "log-terraform-cicd-primus-learning"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
